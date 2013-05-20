b3a48fa1ea590c29f48b6d4efffd10c02996b6e7
apliniogusmao <apliniogusmao@yahoo.com.br>
2013-05-20 17:11:32 -0300

    Criando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compraravistaouaprazo::Application.config.secret_token = '3b13df0ee778e2a9fcd206c14d6e24237429a015634eb3a8973646adf6a981550683b03d4262695a496aa2439436733c1f14bc67c2a9bfd5c841aa398c3fbd70'
