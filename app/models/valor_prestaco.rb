5d3f9bed2ee5cf202c853e5c14b4a9faf762d326
apliniogusmao <apliniogusmao@yahoo.com.br>
2013-05-22 09:40:32 -0300

    Criando regra para valor da prestação.

--- app/models/valor_prestaco.rb
+++ app/models/valor_prestaco.rb
@@ -1,12 +1,11 @@
 class ValorPrestaco < ActiveRecord::Base
-  attr_accessible :numeromeses, :taxajuros, :valorfinanciado
 
-  validates_presence_of :numeromeses, :taxajuros
+  attr_accessible :numeromeses, :taxajuros, :valorfinanciado
   
   def calcular_prestacoes
-    
-    valor_presente = valorfinanciado / (1 + taxajuros) + Math.sqrt(numeromeses)
-    
+
+    valor_presente = valorfinanciado / (1 + taxajuros).sqrt(numeromeses)
+
   end
 
 end
